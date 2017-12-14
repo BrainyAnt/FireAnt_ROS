9b6901b1e66a4cc94eaed1671b593e12148af17b
legit-android-socks <andrei.brumboiu@gmail.com>
2017-12-14 13:13:55 +0200

    Fix firebase stream error. motor_lobe class implementation.

--- ant/scripts/firebase_lobe.py
+++ ant/scripts/firebase_lobe.py
@@ -23,27 +23,23 @@ DB = FIREBASE.database()
 #token = AUTH.create_custom_token("custom_id", {"premium_account": True})
 #user = AUTH.sign_in_with_custom_token(token)
 
-UID = USER['localId']
-OID = 'nCPm7rJBW7N5TWUNPZwsnLOUBzi1'
+OID = 'qYLfxyR6LCMl5czODhiaOc3y18H2'
+RID = '-L-lc4aaO3M17o6SiQhp'
+UID = OID #USER['localId']
 
 MOTION_PUB = rospy.Publisher('motion', String, queue_size=5)
 
-def motion_topic_streamer(robot):
+def motion_topic_streamer():
     """Listen for changes in firebase ControlData"""
     rospy.init_node('firebase_lobe', anonymous=True)
     rate = rospy.Rate(10) #10Hz
-    motion_stream = DB.child("users").child(OID).child("robots").child(robot).child(UID).child("ControlData").order_by_key().stream(stream_handler, None)
+    motion_stream = DB.child("users").child(OID).child("robots").child(RID).child(UID).child("ControlData").order_by_key().stream(stream_handler, None)
     rate.sleep()
-    try:
-        motion_stream.close()
-    except AttributeError:
-        print "ERROR! Stream close"
-        #sys.exit()
-        #pass
+    motion_stream.close()
 
 def get_control_data():
     """Return ControlData values from firebase"""
-    control_data = DB.child("users").child(OID).child("robots").child(ROBOT_NAME).child(UID).child("ControlData").order_by_key().get()
+    control_data = DB.child("users").child(OID).child("robots").child(RID).child(OID).child("ControlData").order_by_key().get()
     return control_data
 
 def stream_handler(message):
@@ -53,25 +49,30 @@ def stream_handler(message):
 
 def robot_set_online():
     """Set field value of isOnline to True"""
-    DB.child('users').child(OID).child('robots').child(ROBOT_NAME).child('profile').child('isOnline').set(True)
+    DB.child('users').child(OID).child('robots').child(RID).child('profile').child('isOnline').set(True)
 
 def robot_set_offline():
     """Set field value of isOnline to False"""
-    DB.child('users').child(UID).child('robots').child(ROBOT_NAME).child('profile').child('isOnline').set(False)
+    DB.child('users').child(OID).child('robots').child(RID).child('profile').child('isOnline').set(False)
 
 def robot_is_online():
     """Return field value of isOnline"""
-    return DB.child('users').child(UID).child('robots').child(ROBOT_NAME).child('profile').child('isOnline').get().val()
+    return DB.child('users').child(OID).child('robots').child(RID).child('profile').child('isOnline').get().val()
+
+def get_robot_name():
+    """Return robot description field value"""
+    name = DB.child('users').child(OID).child('robots').child(RID).child('profile').child('name').get().val()
+    return name
 
 def get_robot_description():
     """Return robot description field value"""
-    description = DB.child('users').child(OID).child('robots').child(ROBOT_NAME).child('profile').child('description').get().val()
+    description = DB.child('users').child(OID).child('robots').child(RID).child('profile').child('description').get().val()
     return description
 
-def share_robot_with_user(robot, user_id, user_name):
-    """Share specified robot with specified user"""
-    data = {'ControlData': {'fwd': 0, 'back': 0, 'left': 0, 'right': 0}, 'user': user_name}
-    DB.child('users').child(OID).child('robots').child(robot).child(user_id).set(data)
+#def share_robot_with_user(robot, user_id, user_name):
+#   """Share specified robot with specified user"""
+#    data = {'ControlData': {'fwd': 0, 'back': 0, 'left': 0, 'right': 0}, 'user': user_name}
+#    DB.child('users').child(OID).child('robots').child(robot).child(user_id).set(data)
 
 if __name__ == '__main__':
 
@@ -82,35 +83,21 @@ if __name__ == '__main__':
     for robot in all_robots.each():
         print(robot.key())
     '''
-    '''
-    print('USERS:')
-    all_users = DB.child("users").order_by_key().get()
-    for user in all_users.each():
-        print(user.key())
-    '''
-    
-    '''
-    userFields = DB.child('users').child('nCPm7rJBW7N5TWUNPZwsnLOUBzi1').child('robots').child('azorel').get()
-    for field in userFields.each():
-        print(field.key())
-    '''
-    '''
-    for x in user:
-        print('{}: {}'.format(x,user[x]))
-    ShareRobotWithUser('azorel', user['localId'], 'Example User')
-    '''
 
     robot_set_online()
     
-    print(UID)
-    print('{}: {}'.format(ROBOT_NAME, get_robot_description()))
+    print("User", UID)
+    print("Owner", OID)
+
+    print('{}: {}'.format(get_robot_name(), get_robot_description()))
+
     print(get_control_data().key())
     for item in get_control_data().val():
         print("{}: {}".format(item, get_control_data().val()[item]))
 
     try:
         while not rospy.is_shutdown():
-            motion_topic_streamer(ROBOT_NAME)
+            motion_topic_streamer()
     except rospy.ROSInterruptException:
         print "ERROR: ROS Interrupted"
     except KeyboardInterrupt:
--- ant/scripts/motor_lobe.py
+++ ant/scripts/motor_lobe.py
@@ -15,37 +15,93 @@ GPIO.setwarnings(False)
 
 #define control pins
 STAND_BY = 38
-MOTORS = collections.namedtuple('motor', 'pwm_pin speed in1 in2')
-LEFT_MOTOR = MOTORS(12, GPIO.PWM(12, 100), 16, 18)
-RIGHT_MOTOR = MOTORS(35, GPIO.PWM(35, 100), 29, 31)
+MOTORS = collections.namedtuple('motor', 'in1 in2 pwm_pin')
+LEFT_MOTOR = MOTORS(16, 18, 12)
+RIGHT_MOTOR = MOTORS(29, 31, 35)
 
-def motor_setup():
-    GPIO.setup(STAND_BY, GPIO.OUT)
-    GPIO.setup(LEFT_MOTOR.pwm_pin, GPIO.OUT)
-    LEFT_MOTOR.speed = GPIO.PWM(LEFT_MOTOR.pwm_pin,100)
-    GPIO.setup(LEFT_MOTOR.in1, GPIO.OUT)
-    GPIO.setup(LEFT_MOTOR.in2, GPIO.OUT)
-    GPIO.setup(RIGHT_MOTOR.pwm_pin, GPIO.OUT)
-    RIGHT_MOTOR.speed = GPIO.PWM(RIGHT_MOTOR.pwm_pin,100)
-    GPIO.setup(RIGHT_MOTOR.in1, GPIO.OUT)
-    GPIO.setup(RIGHT_MOTOR.in2, GPIO.OUT)
+class Motor:
+    def __init__(self, pin1, pin2, pin3, frequency):
+        self.in1 = pin1
+        self.in2 = pin2
+        self.pwm = pin3
+        self.freq = frequency
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        GPIO.setup(self.pwm, GPIO.OUT)
+        self.duty = GPIO.PWM(self.pwm, self.freq)
+        self.duty.start(0)
+    
+    def on(self):
+        self.duty.start(0)
+    
+    def off(self):
+        self.duty.stop()
+
+    def set_forward_dir(self):
+        GPIO.output(self.in1, GPIO.HIGH)
+        GPIO.output(self.in2, GPIO.LOW)
+
+    def set_backward_dir(self):
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.HIGH)
+
+    def run_motor(self, pwm):
+        self.on()
+        if pwm < 0:
+            self.set_backward_dir()
+        else:
+            self.set_forward_dir()
+        self.duty.ChangeDutyCycle(abs(pwm))
+
+class DualMotorDriver:
+    def __init__(self, pin, motor1, motor2):
+        self.standby = pin
+        self.left_motor = motor1
+        self.right_motor = motor2
+
+    def activate(self):
+        self.left_motor.on()
+        self.right_motor.on()
+        GPIO.output(self.standby, GPIO.HIGH)
+
+    def stand_by(self):
+        GPIO.output(self.standby, GPIO.LOW)
+
+    def run_robot(self, left, right, fwd, back):
+        self.activate()
+        #forward or backward?
+        accelleration = (fwd - back) #between [-100,100]
+        #left or right?
+        drift = (right - left) #between [-100,100]
+        #left motor power
+        left_power = (accelleration + drift)*100/(100+abs(drift)) #between [-100,100]
+        #right motor power
+        right_power = (accelleration - drift)*100/(100+abs(drift)) #between [-100,100]
+        self.left_motor.run_motor(left_power)
+        self.right_motor.run_motor(right_power)
+
+    def stop(self):
+        self.stand_by()
+        self.left_motor.off()
+        self.right_motor.off()
+
+motorL = Motor(LEFT_MOTOR.in1, LEFT_MOTOR.in2, LEFT_MOTOR.pwm_pin, 100)
+motorR = Motor(RIGHT_MOTOR.in1, RIGHT_MOTOR.in2, RIGHT_MOTOR.pwm_pin, 100)
+ROBOT = DualMotorDriver(STAND_BY, motorL, motorR)
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + data.data)
     try:
-        #back = int(data.data.split("u'")[0].split(":")[1].split(" ")[1].split(",")[0])
         back = data.data['back']
     except ValueError:
         back = 0
         print("TYPE ERROR: back!")
     try:
-        #fwd = int(data.data.split("u'")[1].split(":")[1].split(" ")[1].split(",")[0])
         fwd = data.data['fwd']
     except ValueError:
         fwd = 0
         print("TYPE ERROR: fwd!")
     try:
-        #left = int(data.data.split("u'")[2].split(":")[1].split(" ")[1].split(",")[0])
         left = data.data['left']
     except ValueError:
         left = 0
@@ -56,61 +112,18 @@ def callback(data):
     except ValueError:
         right = 0
         print("TYPE ERROR: right!")
-    runRobot(left, right, fwd, back)
+    ROBOT.run_robot(left, right, fwd, back)
 
 def motion_topic_listener():
     rospy.init_node('motor_lobe', anonymous=True)
     rospy.Subscriber('motion', String, callback, queue_size=5)
     rospy.spin()
 
-def activate():
-    GPIO.output(STAND_BY, GPIO.HIGH)
-
-def standBy():
-    GPIO.output(STAND_BY, GPIO.LOW)
-
-def setForwardDir(motor):
-    GPIO.output(motor.in1, GPIO.HIGH)
-    GPIO.output(motor.in2, GPIO.LOW)
-
-def setBackwardDir(motor):
-    GPIO.output(motor.in1, GPIO.LOW)
-    GPIO.output(motor.in2, GPIO.HIGH)
-
-def runMotor(motor, pwm):
-    if pwm < 0:
-        setBackwardDir(motor)
-    else:
-        setForwardDir(motor)
-    motor.speed.ChangeDutyCycle(abs(pwm))
-
-def runRobot(left, right, fwd, back):
-    activate()
-    #forward or backward?
-    accelleration = (fwd - back) #between [-100,100]
-    #left or right?
-    drift = (right - left) #between [-100,100]
-    #left motor power
-    left_power = (accelleration + drift)*100/(100+abs(drift)) #between [-100,100]
-    #right motor power
-    right_power = (accelleration - drift)*100/(100+abs(drift)) #between [-100,100]
-    runMotor(LEFT_MOTOR, left_power)
-    runMotor(RIGHT_MOTOR, right_power)
-
-def stop():
-    standBy()
-    GPIO.output(LEFT_MOTOR.in1, GPIO.LOW)
-    GPIO.output(LEFT_MOTOR.in2, GPIO.LOW)
-    LEFT_MOTOR.speed.ChangeDutyCycle(0)
-    GPIO.output(RIGHT_MOTOR.in1, GPIO.LOW)
-    GPIO.output(RIGHT_MOTOR.in2, GPIO.LOW)
-    RIGHT_MOTOR.speed.ChangeDutyCycle(0)
-
-def clear():
-    GPIO.cleanup()
-
 if __name__ == '__main__':
-    motor_setup()
+    ROBOT.activate()
+    ROBOT.stand_by()
+    
     motion_topic_listener()
-    stop()
-    clear()
+    
+    ROBOT.stop()
+    GPIO.cleanup()
